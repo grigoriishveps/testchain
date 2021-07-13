179af3bd4743dae6fd730b9986d3c6fd89affbbf
grigoriishveps <grigorii.shveps@gmail.com>
2021-07-14 01:13:57 +0300

    add components

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -4,10 +4,7 @@ import {Card} from "../Card";
 @Injectable()
 export class DataService{
 
-  private data: Card[] = [{title:"Тест1", content:"afsdfsdf"},
-    {title:"Тест2", content:"adsfsdf"},
-    {title:"Тест3csdafk L", content:"йуа.дтМЛЫВТCSADF'LVLNKSdnvkXNcbmvbjsbkdcl,mzxcvbjsahkeld;lad s dkfbnsjb knak ba kdfbjsj;folbweirajf obewb wqweufhsvdbdkfn,s KDJBLAFJLHWBEK.SNFj.jvlsZDUJK.SL.,ZXFVBQEKAUSYILOI;po:aKFNZV ,N.,AK2LJW;EKIHR[U57384934 23546B3J4VB5HJ4B2N3LWQJREFIL234KWJRO;I3QVOWENF 4JTB 32N 4N 23JV453K4 5KN"},
-    {title:"Тест4", content:"фыв"}];
+  private data: Card[] = [];
   constructor(){}
 
   getData(): Card[] {
